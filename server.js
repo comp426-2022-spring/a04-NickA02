daedf71dd8f4b2dc91f6fdc74638956ef88de638
NickA02 <69744811+NickA02@users.noreply.github.com>
2022-04-12 17:44:45 -0400

    moved middleware to top

--- server.js
+++ server.js
@@ -39,6 +39,24 @@ if (argv.help || argv.h) {
   process.exit(0)
 }
 
+app.use( (req, res, next) => {
+  let logdata = {
+    remoteaddr: req.ip,
+    remoteuser: req.user,
+    time: Date.now(),
+    method: req.method,
+    url: req.url,
+    protocol: req.protocol,
+    httpversion: req.httpVersion,
+    status: res.statusCode,
+    referer: req.headers['referer'],
+    useragent: req.headers['user-agent']
+  }
+  const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+  const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+  next();
+})
+
 app.get('/app/', (req, res) => {
     res.statusCode = 200;
     res.statusMessage = 'OK'
@@ -96,24 +114,6 @@ if (argv.log != 'false') {
   const WRITESTREAM = fs.createWriteStream('access.log', { flags: 'a' })
   app.use(morgan('combined', { stream: WRITESTREAM }))
 }
-
-app.use( (req, res, next) => {
-  let logdata = {
-    remoteaddr: req.ip,
-    remoteuser: req.user,
-    time: Date.now(),
-    method: req.method,
-    url: req.url,
-    protocol: req.protocol,
-    httpversion: req.httpVersion,
-    status: res.statusCode,
-    referer: req.headers['referer'],
-    useragent: req.headers['user-agent']
-  }
-  const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-  const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
-  next();
-})
 
 app.use(function(req, res){
   res.status(404).send('404 NOT FOUND')
